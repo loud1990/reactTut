395e1ed7fe4af915ca82ffe157ceac72109a8909
Lou DiLuzio <loud1990@gmail.com>
2025-01-19 14:38:55 -0500

    added homepage 40:03

--- reactMovieTut/src/App.jsx
+++ reactMovieTut/src/App.jsx
@@ -1,18 +1,14 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Home from './pages/Home';
 
 function App() {
   const movieNumber = 1;
 
   
   return (
-    <>  {/* fragment to encapsulate everything, can only have one parent element inside a function in react*/}
-      {/*conditional rendering below, condition, condition if true, then condition if false after colon*/}
-      {movieNumber === 1 ?( 
-      <MovieCard movie={{title:"Lou's Film", release_date: "2024"}}/>
-    ) : (
-      <MovieCard movie={{title:"Joe's Film", release_date: "2023"}}/>
-    )}
+    <>  
+    <Home />
     </>
   );
 }
