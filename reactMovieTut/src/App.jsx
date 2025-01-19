68332897765ae41b62ac2db6a00be2fc524da8a7
Lou DiLuzio <loud1990@gmail.com>
2025-01-19 14:21:35 -0500

    conditional rendering added

--- reactMovieTut/src/App.jsx
+++ reactMovieTut/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber = 1;
+
+  
   return (
     <>  {/* fragment to encapsulate everything, can only have one parent element inside a function in react*/}
-      <Text display = "whats up"/>
-      <Text display="hello" />
+      {/*conditional rendering below, condition, condition if true, then condition if false after colon*/}
+      {movieNumber === 1 ?( 
+      <MovieCard movie={{title:"Lou's Film", release_date: "2024"}}/>
+    ) : (
+      <MovieCard movie={{title:"Joe's Film", release_date: "2023"}}/>
+    )}
     </>
-  )
+  );
 }
 
 function Text({display}) { {/*define the component in a function here, then in the function above, use the component with the <Text /> tag */}
---
+++ reactMovieTut/src/components/MovieCard.jsx
@@ -0,0 +1,27 @@
+{/* Here we will write the code for displaying a Movie Card*/}
+
+function MovieCard({movie}){
+
+    function onFavoriteClick() {
+
+    }
+
+    return(
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title}/>
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>
+                         <i className="fas fa-heart"></i>
+                     </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
